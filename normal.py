32d1ed41009813c96070592c03e0062bd3c11f4c
Thootau <thootau99@tutanota.com>
2021-12-02 00:57:52 +0900

    Changed location

--- normal.py
+++ normal.py
@@ -31,7 +31,7 @@ class Client():
         self.dialogChecker = DialogChecker(self.battleChecker, self.mqtt)
         
         self.HpMpChecker = HpMpChecker(name, self.mqtt)
-        self.teki = [(52,378), (144,444), (156,326), (220,386), (227,274), (288,335), (269,218), (326,275), (373,163), (432,227)]
+        self.teki = [(52,378), (144,444), (156,326), (220,386), (227,274), (288,335), (269,218), (326,275), (373,163), (434,224)]
         self.dialogLocation = [(384, 310), (384, 341), (384, 367), (384, 400)]
         self.mikata = (588, 460)
         self.name = name
